91574fda38bfc663e9084429b23d3f59100e3df9
ExLyD1 <sashaahnij@gmail.com>
2025-03-25 21:47:47 +0200

    created model optins list Film search

--- features/Film/model/filmsSearchData.ts
+++ features/Film/model/filmsSearchData.ts
@@ -2,11 +2,15 @@ export interface IFilmOptionsList {
 	label: string
 	width: string
 	anyLabel: string
-
+	isSort: boolean
 	options: {
 		option: string | number
 		route_query: string
 		isOptedMore: boolean
+	}[]
+	deep_options?: {
+		label: string
+		options: { option: string | number; route_query: string }[]
 	}[]
 }
 
@@ -15,6 +19,7 @@ export const decadesOptionsList: IFilmOptionsList[] = [
 		label: 'Decade',
 		width: 'w-[130px]',
 		anyLabel: 'Any Decade',
+		isSort: false,
 		options: [
 			{ option: '2020s', route_query: '2020s', isOptedMore: false },
 			{ option: '2010s', route_query: '2010s', isOptedMore: false },
@@ -41,6 +46,7 @@ export const genresOptionsList: IFilmOptionsList[] = [
 		label: 'Genre',
 		width: 'w-[130px]',
 		anyLabel: 'Any genre',
+		isSort: false,
 		options: [
 			{ option: 'Action', route_query: 'action', isOptedMore: true },
 			{ option: 'Adventure', route_query: 'adventure', isOptedMore: true },
@@ -72,28 +78,30 @@ export const sortingOptionsList: IFilmOptionsList[] = [
 		label: 'Genre',
 		width: 'w-[130px]',
 		anyLabel: 'Any genre',
-		options: [
-			{ option: 'Action', route_query: 'action', isOptedMore: true },
-			{ option: 'Adventure', route_query: 'adventure', isOptedMore: true },
-			{ option: 'Animation', route_query: 'animation', isOptedMore: true },
-			{ option: 'Comedy', route_query: 'comedy', isOptedMore: true },
-			{ option: 'Crime', route_query: 'crime', isOptedMore: true },
-			{ option: 'Documentary', route_query: 'documentary', isOptedMore: true },
-			{ option: 'Drama', route_query: 'drama', isOptedMore: true },
-			{ option: 'Family', route_query: 'family', isOptedMore: true },
-			{ option: 'Fantasy', route_query: 'fantasy', isOptedMore: true },
-			{ option: 'History', route_query: 'history', isOptedMore: true },
-			{ option: 'Horror', route_query: 'horror', isOptedMore: true },
-			{ option: 'Music', route_query: 'music', isOptedMore: true },
-			{ option: 'Mystery', route_query: 'mystery', isOptedMore: true },
-			{ option: 'Romance', route_query: 'romance', isOptedMore: true },
-			{ option: 'Science', route_query: 'science', isOptedMore: true },
-			{ option: 'Fiction', route_query: 'fiction', isOptedMore: true },
-			{ option: 'Thriller', route_query: 'thriller', isOptedMore: true },
-			{ option: 'TV', route_query: 'tV', isOptedMore: true },
-			{ option: 'Movie', route_query: 'movie', isOptedMore: true },
-			{ option: 'War', route_query: 'war', isOptedMore: true },
-			{ option: 'Western', route_query: 'western', isOptedMore: true },
+		isSort: true,
+		options: [{ option: 'Film name', route_query: 'name', isOptedMore: false }],
+		deep_options: [
+			{
+				label: 'Release Date',
+				options: [
+					{ option: 'Newest First', route_query: 'newest' },
+					{ option: 'Earliest First', route_query: 'earliest' },
+				],
+			},
+			{
+				label: 'Avarage Rating',
+				options: [
+					{ option: 'Highest First', route_query: 'rating-highest' },
+					{ option: 'Lowest First', route_query: 'rating-lowest' },
+				],
+			},
+			{
+				label: 'Film length',
+				options: [
+					{ option: 'Shortest First', route_query: 'shortest' },
+					{ option: 'Longest First', route_query: 'longest' },
+				],
+			},
 		],
 	},
 ]
